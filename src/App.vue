d2c00b8b2a91abb09e196d3ddef571635609a69d
Saad Khan <saamoz2012@gmail.com>
2020-04-23 21:51:55 -0400

    Started work on the basic Vue structure

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <ul class="nav navbar-nav">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view class="view"></router-view>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+const Home = () => import("./views/Home.vue");
+
+const router = new VueRouter({
+  mode: "history",
+  base: __dirname,
+  routes: [
+    { path: "/", component: Home},
+    { path: "/test", component: Home}
+  ]
+});
+
+export default {
+  name: 'App',
+  router: router
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="col-lg-9 jumbotron">
+            <h1>Welcome to the Survey</h1>
+            <p>
+                Test
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import 'bootstrap/dist/css/bootstrap.css'
+    console.log("TEST")
+
+    export default {};
+</script>
