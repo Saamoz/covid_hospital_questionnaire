05cd6a9bb27289869feb16aba50707c7c84d4aaf
Saad Khan <saamoz2012@gmail.com>
2020-04-24 20:00:30 -0400

    Updated survey questions

--- src/views/Staff.vue
+++ src/views/Staff.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <h2>Direct Exposure Questionnaire</h2>
-        <h4>Complete this if you were directly exposed to COVID-19 from a relative or a friend</h4>
+        <h4>Complete this if you were exposed to someone who was exposed to a confirmed case of COVID-19</h4>
         <survey :survey="survey"></survey>
     </div>
 </template>
@@ -13,6 +13,10 @@
     var Survey = SurveyVue.Survey
     Survey.cssType = "bootstrap";
 
+    const lessExposed = "<h4>Complete the following steps:</h4> <ol> <li>Continue duties</li> <li>No test is required</li> <li>Active monitoring for 14 days</li> </ol>"
+    // eslint-disable-next-line no-unused-vars
+    const moreExposed = "<h4>Complete the following steps:</h4> <ol> <li>Stop duties</li> <li>Test is required</li> <li>Isolation for 14 days</li> </ol>"
+
     export default {
         name: 'app',
         components: {
@@ -21,67 +25,96 @@
         data () {
             var json = {
                 "showNavigationButtons": "none",
+                clearInvisibleValues: "onHidden",
                 questions: [
                     {   type: "dropdown",
-                        name: "exposureType",
-                        title: "How were you exposed?",
+                        name: "firstTest",
+                        title: "What were the test results of the person you were exposed to tested?",
                         choices: [
-                            "Direct Exposure (Test Positive)",
-                            "Indirect Exposure (Symptomatic)",
-                            "Indirect Exposure (Asymptomatic)",
-                            "In Contact with Indirect Exposure"
+                            {value: 1, text: "Test Positive"},
+                            {value: 2, text: "Test Negative"}
                         ]
                     },
-                    {
-                        type: "html",
-                        name: "asympExposure",
-                        html: "<h4>Follow all the following steps:</h4> <ol> <li>Continue duties with mask</li> <li>No test is required</li> <li>Active monitoring for 14 days</li> </ol>",
-                        visibleIf: "{exposureType}='Indirect Exposure (Asymptomatic)'"
-                    },
-                    {
-                        type: "dropdown",
-                        name: "exposureType2",
-                        title: "What type of exposure?",
+                    {   type: "dropdown",
+                        name: "negativeSymptoms",
+                        title: "Are you experiencing any symptoms?",
                         choices: [
-                            "Test Positive",
-                            "Test Negative"
+                            {value: 1, text: "Yes, I'm experiencing symptoms"},
+                            {value: 2, text: "Yes, I'm not experiencing symptoms"}
                         ],
-                        visibleIf: "{exposureType}='In Contact with Indirect Exposure'"
+                        visibleIf: "{firstTest}='2'"
                     },
                     {
                         type: "html",
-                        name: "exposureNegative",
-                        html: "<h4>Follow all the following steps:</h4> <ol> <li>Continue duties</li> <li>No test is required</li> <li>Active monitoring for 14 days</li> </ol>",
-                        visibleIf: "{exposureType2}='Test Negative'"
+                        name: "negNoSymptoms",
+                        html: moreExposed,
+                        visibleIf: "{negativeSymptoms}='1'"
                     },
                     {
                         type: "html",
-                        name: "testPositive",
-                        html: "<h4>Follow all the following steps:</h4> <ol> <li>Stop duties</li> <li>Test is required</li> <li>Isolation for 14 days</li> </ol>",
-                        visibleIf: "{exposureType2}='Test Positive' or {exposureType}='Direct Exposure (Test Positive)' or {exposureType}='Indirect Exposure (Symptomatic)'"
+                        name: "negNoSymptoms",
+                        html: lessExposed,
+                        visibleIf: "{negativeSymptoms}='2'"
                     },
-                    {
-                        type: "dropdown",
-                        name: "testAgain",
-                        title: "Test again",
+                    {   type: "dropdown",
+                        name: "exposureRisk",
+                        title: "Was your exposure low risk?",
                         choices: [
-                            "Test Positive",
-                            "Test Negative"
+                            {value: 1, text: "My exposure was low risk (e.g. contact < 20 mins, distance > 1.5m, wearing mask)"},
+                            {value: 2, text: "My exposure was high risk (e.g. contact > 20 mins, distance < 1.5m, not wearing mask)"}
                         ],
-                        visibleIf: "{exposureType2}='Test Positive' or {exposureType}='Direct Exposure (Test Positive)' or {exposureType}='Indirect Exposure (Symptomatic)'"
+                        visibleIf: "{firstTest}='1'"
                     },
                     {
                         type: "html",
-                        name: "testPositive2",
-                        html: "<h4>Return to work after:</h4> <ol> <li>Isolation for 14 days post exposure</li> <li>Repeat test on day 13 post exposure</li> </ol>",
-                        visibleIf: "{testAgain}='Test Positive'"
+                        name: "negNoSymptoms",
+                        html: moreExposed,
+                        visibleIf: "{exposureRisk}='2'"
                     },
                     {
                         type: "html",
-                        name: "testNegative2",
-                        html: "<h4>Return to work after:</h4> <ol> <li>Asymptomatic for 48 hours</li> <li>Isolation for 14 days post exposure</li> <li>At least 2 negative tests for COVID-19</li> </ol>",
-                        visibleIf: "{testAgain}='Test Negative'"
-                    }
+                        name: "negNoSymptoms",
+                        html: lessExposed,
+                        visibleIf: "{exposureRisk}='1'"
+                    },
+                    {   type: "dropdown",
+                        name: "meetingPeople",
+                        title: "After exposure did you mix with people at work?",
+                        choices: [
+                            {value: 1, text: "Yes, I mixed with people"},
+                            {value: 2, text: "No, I did not mix with people"}
+                        ],
+                        visibleIf: "{exposureRisk} notempty or {negativeSymptoms} notempty"
+                    },
+                    {
+                        type: "html",
+                        name: "didNotMeet",
+                        html: "<h3>Good Job. Make sure you follow the instructions above</h3>",
+                        visibleIf: "{meetingPeople}='2'"
+                    },
+                    {   type: "dropdown",
+                        name: "youTested",
+                        title: "Are you tested for COVID-19??",
+                        choices: [
+                            {value: 1, text: "Yes, I'm tested"},
+                            {value: 2, text: "No, I'm not tested"}
+                        ],
+                        visibleIf: "{meetingPeople}=1"
+                    },
+                    {
+                        type: "html",
+                        name: "didNotTest",
+                        html: "<h4>Other people exposed to you should follow these instructions</h4> <ol> <li>Continue duties</li> <li>No test is required</li> <li>Active monitoring for 14 days</li> </ol>",
+                        visibleIf: "{youTested}='2'"
+                    },
+                    {
+                        type: "html",
+                        name: "didTest",
+                        html: "<h4>Other people exposed to you should follow these instructions</h4> <ol> <li>Stop duties</li> <li>Test is required</li> <li>Isolation for 14 days</li> </ol>",
+                        visibleIf: "{youTested}='1'"
+                    },
+
+
                 ]
             };
 
